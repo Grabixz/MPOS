5877872c1b6fb525d2b14d0a465a05ff0f2dc00f
Joshua Personal <joshua.commerford@gmail.com>
2026-03-27 11:22:39 +0200

    New updates

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,17 @@
 import { Stack } from "expo-router";
 import '@/global.css';
+import { CartProvider } from "@/context/cartContext";
+import { WishlistProvider } from "@/context/wishListContext";
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 export default function RootLayout() {
-  return <Stack screenOptions={{ headerShown: false }}/>;
+  return (
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <CartProvider>
+        <WishlistProvider>
+          <Stack screenOptions={{ headerShown: false }}/>
+        </WishlistProvider>
+      </CartProvider>
+    </GestureHandlerRootView>
+  )
 }
--- components/productCard.tsx
+++ components/productCard.tsx
@@ -4,10 +4,12 @@ import { ProductCardProps } from '@/constants/types'
 import { Link } from 'expo-router'
 import { Ionicons } from '@expo/vector-icons'
 import { COLORS } from '@/constants'
+import { useWishlist } from '@/context/wishListContext'
 
 export default function ProductCard({product}: ProductCardProps) {
+    const {toggleWishlist, isInWishlist} = useWishlist()
 
-    const isLiked = false
+    const isLiked = isInWishlist(product._id);
 
   return (
     <Link href={`/product/${product._id}`} asChild>
@@ -15,7 +17,7 @@ export default function ProductCard({product}: ProductCardProps) {
             <View className='relative h-56 w-full bg-gray-100'>
                 <Image source={{uri: product.images[0]}} className='w-full h-full' resizeMode='cover'/>
                 {/* Favourite Icon */}
-                <TouchableOpacity className='absolute top-2 right-2 z-10 p-2 bg-white rounded-full' onPress={(e) => {e.stopPropagation();}}>
+                <TouchableOpacity className='absolute top-2 right-2 z-10 p-2 bg-white rounded-full' onPress={(e) => {e.stopPropagation(); toggleWishlist(product)}}>
                     <Ionicons name={isLiked ? 'heart' : 'heart-outline'} size={20} color={isLiked ? COLORS.accent : COLORS.primary} />
                 </TouchableOpacity>
 
